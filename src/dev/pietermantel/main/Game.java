41434a781cd9150360abfb8cd06d3a12754335f7
HeikoRibberink <47252922+HeikoRibberink@users.noreply.github.com>
2019-02-01 18:04:28 +0100

    newMain

--- src/dev/pietermantel/main/Game.java
+++ src/dev/pietermantel/main/Game.java
@@ -3,11 +3,11 @@ package dev.pietermantel.main;
 public class Game {
 
 	public Game() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Game();
 
 	}
 
