2b97add9f8646ea2357c832d4c5d5e7ae7c362a6
pietermantel <2pietermantel@gmail.com>
2019-02-04 16:02:02 +0100

    Background #2

--- src/dev/pietermantel/background/Background.java
+++ src/dev/pietermantel/background/Background.java
@@ -1,5 +1,38 @@
 package dev.pietermantel.background;
 
+import java.awt.Image;
+import java.io.IOException;
+
+import dev.pietermantel.main.ResourceLoader;
+
 public enum Background {
+	playersBedroom("res/backgrounds/players_bedroom.jpg");
 	
+	private String imagePath;
+	private Image image;
+	
+	private Background(String imagePath) {
+		this.imagePath = imagePath;
+		try {
+			image = ResourceLoader.loadTexture(imagePath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+	}
 }
