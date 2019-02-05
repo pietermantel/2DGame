c9568236cf0526bb6d40c2c313734eb76055b6b1
HeikoRibberink <47252922+HeikoRibberink@users.noreply.github.com>
2019-02-05 16:58:04 +0100

    Scroll Borders #3

--- src/dev/pietermantel/object/instances/Player.java
+++ src/dev/pietermantel/object/instances/Player.java
@@ -27,23 +27,21 @@ public class Player extends GameObject {
 
 	@Override
 	public void render(Graphics g) {
-		x = (Game.GAME.getWindow().getCanvas().getWidth() / 2 - width) - (BackgroundHandler.SCROLL_X - BackgroundHandler.ACTUAL_X);
-		y = (Game.GAME.getWindow().getCanvas().getHeight() / 2 - height) - (BackgroundHandler.SCROLL_Y - BackgroundHandler.ACTUAL_Y);
+		x = (Game.GAME.getWindow().getCanvas().getWidth() / 2 - width / 2) - (BackgroundHandler.SCROLL_X - BackgroundHandler.ACTUAL_X);
+		y = (Game.GAME.getWindow().getCanvas().getHeight() / 2 - height / 2) - (BackgroundHandler.SCROLL_Y - BackgroundHandler.ACTUAL_Y);
 		if (x < 0) {
 			x = 0;
-			BackgroundHandler.SCROLL_X = Game.GAME.getWindow().getCanvas().getWidth() / 2 - width;
+			BackgroundHandler.SCROLL_X = Game.GAME.getWindow().getCanvas().getWidth() / 2 - width / 2;
 		}
 		if (y < 0) {
 			y = 0;
-			BackgroundHandler.SCROLL_Y = Game.GAME.getWindow().getCanvas().getHeight() / 2 - height;
+			BackgroundHandler.SCROLL_Y = Game.GAME.getWindow().getCanvas().getHeight() / 2 - height / 2;
 		}
 		if (x > Game.GAME.getWindow().getCanvas().getWidth() - width) {
 			x = Game.GAME.getWindow().getCanvas().getWidth() - width;
-//			BackgroundHandler.SCROLL_X = BackgroundHandler.SCROLL_X = Game.GAME.getWindow().getCanvas().getWidth() / 2 - Width;
 		}
 		if (y > Game.GAME.getWindow().getCanvas().getHeight() - height) {
 			y = Game.GAME.getWindow().getCanvas().getHeight() - height;
-//			BackgroundHandler.SCROLL_Y = BackgroundHandler.SCROLL_Y = Game.GAME.getWindow().getCanvas().getHeight() / 2 - Height;
-	}
+		}
 	}
 }
